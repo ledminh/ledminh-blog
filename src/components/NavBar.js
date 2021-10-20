3f25e2eec90125ac3a2ab7a5611516b710e17458
Minh Le <minh.le@sjsu.edu>
2021-10-19 22:19:53 -0400

    Add section to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,19 +5,25 @@ const NavBar = () => {
     return (
       <nav className="top-nav-bar">
         <div className="menu-title">Menu</div>
-        <ul>
-            <li>Home</li>
-            <li>Posts</li>
-            <li>About Me</li>
-            <li>Contact Me</li>
-        </ul>           
-        <ul>
-            <li>F</li>
-            <li>T</li>
-            <li>G</li>
-            <li>I</li>
-        </ul>
-        <input type="text" placeholder="Search.."/>
+        <section className="main-menu">
+            <ul>
+                <li>Home</li>
+                <li>Posts</li>
+                <li>About Me</li>
+                <li>Contact Me</li>
+            </ul>           
+        </section>
+        <section className="social-media-menu">
+            <ul>
+                <li>F</li>
+                <li>T</li>
+                <li>G</li>
+                <li>I</li>
+            </ul>
+        </section>
+        <section className="search-bar">
+            <input type="text" placeholder="Search.."/>
+        </section>
       </nav>
     );
 }
