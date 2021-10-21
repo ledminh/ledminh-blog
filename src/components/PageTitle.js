7f763fb9cb326b5c8c7e31e3847d08306d90f2c2
Minh Le <minh.le@sjsu.edu>
2021-10-20 22:51:43 -0400

    Add animation to page title

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -4,8 +4,10 @@ const PageTitle = () => {
 
     return (
       <div className="page-title">
-        <div className="title">LEDMINH</div>
-        <div className="subtitle">A passionate developer</div>
+        <a href="#">
+          <div className="title">LE<span className="hidden-characters"> </span>D<span className="hidden-characters">UY </span>MINH</div>
+          <div className="subtitle">A passionate developer</div>
+        </a>
       </div>
     );
 }
