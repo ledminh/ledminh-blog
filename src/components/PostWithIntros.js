c8c5d929b5ac5e48ceaad01cb30066de4a8fc9eb
Minh Le <minh.le@sjsu.edu>
2021-10-26 16:29:30 -0400

    Add link-on-title class and fix PostWithIntros

--- src/components/PostWithIntros.js
+++ src/components/PostWithIntros.js
@@ -139,11 +139,27 @@ const MainPost = ({title, feature_image_url,
             <img src={feature_image_url} className="feature-image" />
             <div className="title">{title}</div>
             <section className="meta-data">
-                <span className="categories">{categories.map(c => <span className="item">{c}</span>)}</span>
-                <span className="tags">{tags.map(t => <span className="item">{t}</span>)}</span>
-                <span className="date-created">{date_created}</span>
-                <span className="comment-link" onClick={() => setShowComments(true)}>{comments.length} Comments</span>
-                <span className="author">{author}</span>
+                <span className="categories">
+                    {
+                        categories.map((c,i) => 
+                            <span className="item">
+                                <span className="link-on-title">{c}</span>
+                                <span>{(i == categories.length - 1)? "" : ","}</span>
+                            </span>)
+                    }
+                </span>    
+                <span className="tags">
+                    {
+                        tags.map((t,i) => 
+                            <span className="item">
+                                <span className="link-on-title">{t}</span>
+                                <span>{(i == tags.length - 1)? "" : ","}</span>
+                            </span>)
+                    }
+                </span>
+                <span className="date-created"><span className="link-on-title">{date_created}</span></span>
+                <span className="comment-link link-on-title" onClick={() => setShowComments(true)}>{comments.length} Comments</span>
+                <span className="author"><span className="link-on-title">{author}</span></span>
             </section>
             <section className={"excerpt" + (showFullPost? " hidden" : " show")}>
                 {excerpt}
