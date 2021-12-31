dedd301b6c98a7cfd024433bd8dcc87d7535f518
Minh Le <minh.le@sjsu.edu>
2021-12-31 13:08:57 -0500

    Move AuthorPage feature image like to asset

--- src/components/blog/assets/imageLinks.js
+++ src/components/blog/assets/imageLinks.js
@@ -2,3 +2,4 @@ export const CategoryImageURL ="https://cdn.searchenginejournal.com/wp-content/u
 export const TagsImageURL = "https://cdn5.vectorstock.com/i/1000x1000/28/19/set-blank-vintage-frames-gift-tags-labels-vector-13982819.jpg";
 export const DatesImageURL = "https://www.saratogian.com/wp-content/uploads/2021/11/calendar.jpg";
 export const AuthorListsImageURL = "https://s26162.pcdn.co/wp-content/uploads/2018/02/writing-group.jpg";
+export const AuthorPageImageURL = "https://collegeinfogeek.com/wp-content/uploads/2020/05/writing-apps-featured-document-on-computer.jpg";
